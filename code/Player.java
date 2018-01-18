5e6328a77e1442967b85b2b6e3ad39fe2098021f
valawilliams <v.williams3@lancaster.ac.uk>
2018-01-18 08:58:17 +0000

    add javadoc comments; remove unnecessary includes

--- code/Player.java
+++ code/Player.java
@@ -1,13 +1,10 @@
-//
-//import java.util.function.*;
-import java.lang.*;
 import java.nio.file.*;
 import java.io.*;
 
 import org.jsfml.system.*;
-import org.jsfml.window.*;
-import org.jsfml.window.event.*;
-import org.jsfml.window.Keyboard.*;
+//import org.jsfml.window.*;
+//import org.jsfml.window.event.*;
+//import org.jsfml.window.Keyboard.*;
 import org.jsfml.graphics.*;
 
 class Player
@@ -16,6 +13,9 @@ class Player
 	Vector2i size;
 	Vector2f position;
 	
+	/**
+	 * Player constructor - creates a player sprite
+	 */
 	public Player()
 	{
 		Texture texture = new Texture();
@@ -30,31 +30,47 @@ class Player
 			System.out.println("Unable to open texture file");
 		}
 
-
 		sprite = new Sprite(texture);
 
-		//Set its origin to its centre and put it on the screen
+		// set its origin to its centre and put it on the screen
 		sprite.setOrigin(0,0);
 		sprite.setPosition(512-(size.x/2), 384);
-		System.out.println("Y poistion of Sprite: " + this.getYBottomPosition());
+		System.out.println("Y position of Sprite: " + this.getYBottomPosition());
 	}
 
+	/**
+	 * getSprite - returns the shape object for this player
+	 * @return Sprite, the shape object
+	 */
 	public Sprite getSprite()
 	{
 		return sprite;
 	}
 		
+	/**
+	 * move - moves the player by the specified amount
+	 * @param xInc - the increment to move on the x-axis
+	 * @param yInc - the increment to move on the y-axis
+	 */
 	public void move(int xInc, int yInc)
 	{
 		sprite.move(xInc, yInc);
 	}
 	
+	/**
+	 * getYBottomPosition - returns the yPosition of the bottom edge of the player
+	 * @return int - the position of the bottom edge of the player
+	 */
 	public int getYBottomPosition()
 	{
 		position = sprite.getPosition();
 		return Math.round(position.y + size.y);
 	}
 	
+	/**
+	 * touching - returns true if any part of the player is vertically touching the specified position
+	 * @return boolean - returns true if touching
+	 */
 	public boolean touching(int y)
 	{
 		//if player yposition + player height >= yInc
