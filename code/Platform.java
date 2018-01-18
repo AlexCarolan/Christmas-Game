8d7abb1fb502d6e7985d7c0abf0dedb09e6018b5
valawilliams <v.williams3@lancaster.ac.uk>
2018-01-18 08:47:33 +0000

    add javadoc comments; remove unnecessary includes

--- code/Platform.java
+++ code/Platform.java
@@ -1,19 +1,20 @@
-//
-//import java.util.function.*;
-import java.lang.*;
-import java.nio.file.*;
-import java.io.*;
-
 import org.jsfml.system.*;
-import org.jsfml.window.*;
-import org.jsfml.window.event.*;
-import org.jsfml.window.Keyboard.*;
+//import org.jsfml.window.*;
+//import org.jsfml.window.event.*;
+//import org.jsfml.window.Keyboard.*;
 import org.jsfml.graphics.*;
 
 class Platform 
 {
 	RectangleShape plat;
 	
+	/**
+	 * Platform constructor - creates a platform (for the sprite to stand on)
+	 * @param xSize - the width of the platform
+	 * @param ySize - the height of the platform
+	 * @param xPosition - the left hand side of the platform
+	 * @param yPosition - the top of the platform
+	 */
 	public Platform(int xSize, int ySize, int xPosition, int yPosition)
 	{
 		// create a rectangle shape
@@ -22,11 +23,20 @@ class Platform
 		plat.setPosition(xPosition,yPosition);
 	}
 	
+	/**
+	 * getPlatform - returns the shape object for this platform
+	 * @return RectangleShape, the shape object
+	 */
 	public RectangleShape getPlatform()
 	{
 		return plat;
 	}
 	
+	/**
+	 * move - moves the platform by the specified amount
+	 * @param xInc - the increment to move on the x-axis
+	 * @param yInc - the increment to move on the y-axis
+	 */
 	public void move(int xInc, int yInc)
 	{
 		plat.move(xInc, yInc);
