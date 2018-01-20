e109b6d6824ce1ba69043b5892db9212d716d7fc
valawilliams <v.williams3@lancaster.ac.uk>
2018-01-20 13:55:11 +0000

    add back changes lost in last commit

--- code/Animation.java
+++ code/Animation.java
@@ -1,25 +1,29 @@
 /**
- * This class creates the animation for the player
+ * This class animates a player in the game
  */
 import java.nio.file.*;
 import java.io.*;
 import org.jsfml.graphics.*;
 
+import java.util.ArrayList;
+
 class Animation extends Thread
 {
 	private Player player;
-	private Texture[] texture = new Texture[4];
+	private Texture[] texture;
 	
 	/**
 	 * Animation constructor - create a set of textures (images) for this player
-	 * @param p - player
+	 * @param p - the player instance to be animated
 	 * @param path - file path for texture (image); each texture has the same file name followed by a number starting from 1
+	 * @param frames - the number of frames in the animation
 	 */
-	public Animation(Player p, String path)
+	public Animation(Player p, String path, int frames)
 	{
 		player = p;
+		texture = new Texture[frames];
 		
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < frames; i++)
 		{
 			texture[i] = new Texture();
 			try {
@@ -39,7 +43,7 @@ class Animation extends Thread
 	{
 		while (true)
 		{
-			for (int i = 0; i < 4; i++)
+			for (int i = 0; i < texture.length; i++)
 			{
 				player.setSprite(texture[i]);
 				
