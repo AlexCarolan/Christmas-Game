6571f9c07dd2e3dadea215ea2a09ada479ab32d8
Alex Carolan <AlexCarolan0@gmail.com>
2018-01-20 13:24:53 +0000

    Added javadoc comments and support for animations of varying length

--- code/Animation.java
+++ code/Animation.java
@@ -1,18 +1,28 @@
+/**
+ * This class animates a player in the game
+ */
 import java.nio.file.*;
 import java.io.*;
 import org.jsfml.graphics.*;
+import java.util.ArrayList;
 
 class Animation extends Thread
 {
 	private static Player player;
-	private static final Texture[] texture = new Texture[4];
+	private Texture[] texture;
 	
-	
-	public Animation(Player p, String path)
+	/**
+	 * Platform constructor - creates a platform (for the sprite to stand on)
+	 * @param p - the player instance to be animated
+	 * @param path - the file path to the sprites
+	 * @param frames - the number of frames in the animation
+	 */
+	public Animation(Player p, String path, int frames)
 	{
 		player = p;
+		texture = new Texture[frames];
 		
-		for(int i=0; i<4; i++)
+		for(int i=0; i<frames; i++)
 		{
 			texture[i] = new Texture();
 			try {
@@ -22,14 +32,16 @@ class Animation extends Thread
 				System.out.println("Unable to open image file");
 			}
 		}
-		
 	}
 	
+	/**
+	* Starts the thread causing the frames to cycle at regular intervals
+	*/
 	public void run()
   {
 		while(true)
 		{
-			for(int i=0; i<4; i++)
+			for(int i=0; i<texture.length; i++)
 			{
 				player.setSprite(texture[i]);
 				
